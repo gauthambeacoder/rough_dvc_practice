46b3f4f6dbf71d1901b1c576d6c03e7abb79c398
gautham <gauthamb735@gmail.com>
2025-08-05 15:46:01 +0530

    first commit before csv

---
+++ dvc_pratice.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# Create a dictionary
+data = {
+    'name': ['Alice', 'Bob', 'Charlie', 'David', 'Eva'],
+    'age': [25, 30, 22, 28, 35],
+    'city': ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix']
+}
+
+# Convert to DataFrame
+df = pd.DataFrame(data)
+
+
+
